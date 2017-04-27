51a58a4e1d07078f2c296a33982145e566e1ea07
=sagargore2893 <sagargore280193@gmail.com>
2017-04-27 14:40:04 +0530

    header changes1

--- copyQTEEPro/src/test/java/com/mycompany/demoqt/Pagelibrary/Header.java
+++ copyQTEEPro/src/test/java/com/mycompany/demoqt/Pagelibrary/Header.java
@@ -10,10 +10,17 @@ import com.mycompany.demoqt.testbase.TestBase;
 public class Header extends TestBase {
 
 	//WebDriver driver;
+	@FindBy(xpath="//a[@class='search-show']")
+	private  WebElement search;
 	
 	@FindBy(xpath="//a[@href='/account/login']/i")
 	private WebElement login;
 
+	@FindBy(xpath="//a[@class='custom-font slide-menu slide-menu-cart']/i")
+	private WebElement cart;
+	
+	
+	//initialize the object using constructor
 	public Header(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(this.driver, this);
@@ -23,6 +30,16 @@ public class Header extends TestBase {
 	{
 		login.click();
 		Thread.sleep(5000);
+	}
+	
+	public void clickOnCart()
+	{
+		cart.click();
+	}
+	
+	public void clickOnSearch()
+	{
+		search.click();
 	}
 	
 	
